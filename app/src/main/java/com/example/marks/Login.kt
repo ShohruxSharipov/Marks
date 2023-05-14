ed9097db7197f74c8bc30843cd465769a40b8584
User <shoxruxsharipov1105@gmail.com>
2023-05-14 21:07:55 +0500

    second commit

--- app/src/main/java/com/example/marks/Login.kt
+++ app/src/main/java/com/example/marks/Login.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.marks.databinding.FragmentLoginBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +34,12 @@ class Login : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val binding = FragmentLoginBinding.inflate(inflater,container,false)
+
+        binding.register.setOnClickListener{
+            parentFragmentManager.beginTransaction().replace(R.id.main,Registration()).commit()
+        }
+        return binding.root
     }
 
     companion object {
